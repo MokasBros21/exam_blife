8b2eaf1f6ff1989fc15ff4988d333b4544c67925
Oswaldo Hernández <ohernandez@trafficlight.mx>
2024-04-19 09:34:57 -0600

    New Build

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import 'semantic-ui-css/semantic.min.css'
 
 import {
-	Routes, Route, BrowserRouter
+	Routes, Route, BrowserRouter, Navigate
 } from "react-router-dom";
 
 import { rutas } from './routes';
@@ -12,6 +12,10 @@ function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
+				<Route
+					path="/"
+					element={<Navigate to="/promocion" replace />}
+				/>
 				{
 					rutas.map(
 						(ruta, indice) => {
